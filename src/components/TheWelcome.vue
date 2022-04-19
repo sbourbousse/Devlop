412d659889bcb7a77d2326ac0ad56d3ee21d63ac
Sylvain <sylvaindu05@yahoo.fr>
2022-04-19 21:35:56 +0200

    Initial commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+
+function myAlert() {
+  let responseOfUser = confirm("Hello World!");
+}
+
+</script>
+
+<template>
+  <!-- <WelcomeItem>
+  </WelcomeItem> -->
+
+  <button class="my-button"  v-on:click="myAlert()">
+    Click Me
+  </button>
+
+</template>
+
+
+<style scoped>
+  .my-container {
+    background-color: red;
+    font-size: 20px;
+
+  }
+
+  .my-button {
+
+  }
+</style>
---
+++ src/components/WelcomeItem.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>WelcomeItem</h1>
+</template>
+
+<style scoped>
+
+</style>
